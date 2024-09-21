a4f739c65ab1c88288ba58dd266ca76c9a36f42a
JhoncsQA <jhoncabezas@unicomfacauca.edu.co>
2024-09-20 21:47:29 -0500

    Se crea app.s con setup reactivo

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const {createApp, ref}= Vue;
+const app = createApp({
+
+    template:`
+    <h1>Hola Mundo</h1>
+    <p>desde app.js<p>
+    
+    {{vari}}
+
+    `,
+    setup(){
+        console.log('Hola jhon')
+        let variable = 2;
+        const vari = ref("50");      
+
+        setTimeout(() => {
+            vari.value="20";
+        }, 1000);
+
+        return {vari}
+    }
+    
+    
+});
+
+app.mount('#jhon');
